f973536e14666b7165080e50860ab99b1ffc618d
Parth Sarathi Dixit <dixitparthsarathi@gmail.com>
2024-01-08 18:54:02 +0530

    Add Readme

--- LC-24/BinarySearch704.java
+++ LC-24/BinarySearch704.java
@@ -10,7 +10,7 @@ public class BinarySearch704 {
         int n = nums.length;
         int low = 0, high = n - 1;
         while (low <= high) {
-            int mid = (low + high) / 2;
+            int mid = low + (high - low) / 2;
             if (nums[mid] == target)
                 return mid;
             else if (target > nums[mid])
@@ -19,5 +19,22 @@ public class BinarySearch704 {
                 high = mid - 1;
         }
         return -1;
+    }
+
+    public static int searchRecursive(int[] nums, int target, int low, int high) {
+        int n = nums.length;
+        low = 0;
+        high = n - 1;
+        if (low > high) {
+            return -1;
+        }
+        int mid = low + (high - low) / 2;
+        if (target == nums[mid]) {
+            return mid;
+        } else if (target > nums[mid]) {
+            return searchRecursive(nums, target, mid + 1, high);
+        } else {
+            return searchRecursive(nums, target, low, mid - 1);
+        }
     }
 }
